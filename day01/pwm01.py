7db0e60393dab43387bf8265cc92585f5e308597
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-20 16:12:43 +0900

    Update pwm01.py

--- day01/pwm01.py
+++ day01/pwm01.py
@@ -9,12 +9,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(piezoPin, GPIO.OUT)
 
 # 아날로그 출력을 위한 객체 생성(440Hz로 출력)
-buzz = GPIO.PWM(piezoPin, 440)
+Buzz = GPIO.PWM(piezoPin, 440)
 
 try:
   while True:
     Buzz.start(50)    # duty cycle: 50 (50의 시간을 주며 동작)
-    for i in range(0, len(melody):
+    for i in range(0, len(melody)):
       BUzz.ChangeFrequency(melody[i])
       time.sleep(0.3)
     Buzz.stop()
