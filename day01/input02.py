00b7d4333c483f308f4852ea87c689c542a0b9f2
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-20 16:52:27 +0900

    Update input02.py

--- day01/input02.py
+++ day01/input02.py
@@ -2,15 +2,28 @@ import RPi.GPIO as GPIO
 import time
 
 led = 18
+piezoPin = 13
+melody = [130, 147, 165, 175, 196, 220, 247, 262]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led, GPIO.OUT)
+GPIO.setup(piezoPin, GPIO.OUT)
+
+Buzz = GPIO.PWM(piezoPin, 440)
 
 try:
-  while True:
-    a = input()
-    if a == '0':
-      GPIO.output(led, False)
-      
+    while True:
+        a = input()
+        if a == 'O':
+            GPIO.output(led, False)
+        elif a == 'X':
+            GPIO.output(led, True)
+        elif a.isdigit() and 1 <= int(a) <= 8:  
+            index = int(a) - 1  
+            Buzz.start(50)
+            Buzz.ChangeFrequency(melody[index])
+        elif a == 'Q':
+            Buzz.stop()
+
 except KeyboardInterrupt:
-  GPIO.cleanup()    
+    GPIO.cleanup()
