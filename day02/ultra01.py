ad1e744af38dd5ad07c6e83d29062490e9ae4435
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-21 11:47:11 +0900

    Create ultra01.py

---
+++ day02/ultra01.py
@@ -0,0 +1,35 @@
+# Ultra
+import RPi.GPIO as GPIO
+import time
+
+def measure():
+  GPIO.output(trigPin, True)  # 10us 동안 high 레벨로 triger를 출력하여 초음파 발생 준
+  time.sleep(0.00001)
+  GPIO.output(trigPin, False)
+  start = time.time()            # 현재시간 저장
+
+  While GPIO.input(echoPin) == False:  # echo가 없으면
+    start = time.time()                # 현재 시간을 start 변수에 저장하고
+  While GPIO.input(echoPin) == True:   # echo가 있으면
+    stop = time.time()                 # 현재 시간을 stop 변수에 저장
+  elapsed = stop - start               # 걸린 시간을 구하고
+  distance = (dlapsed * 19000) / 2     # 초음파 속도를 이용해서 거리 계산
+
+  return distance                       # 거리 반환
+
+# 핀 설정
+trigPin = 0
+echoPin = 1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(trigPin, GPIO.OUT)
+GPIO.setup(echoPin, GPIO.IN)
+
+try:
+  while True:
+    distance = measure():
+    print("Distance: %.2f cm" %distance)
+    time.sleep(1)
+
+except KeyboardInterrupt:
+  GPIO.cleanup()
