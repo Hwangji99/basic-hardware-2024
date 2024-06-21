ac4c8047d4c459ab85def4f164faa8d5e7268d5c
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-21 11:56:47 +0900

    Update ultra01.py

--- day02/ultra01.py
+++ day02/ultra01.py
@@ -13,7 +13,7 @@ def measure():
   while GPIO.input(echoPin) == True:   # echo가 있으면
     stop = time.time()                 # 현재 시간을 stop 변수에 저장
   elapsed = stop - start               # 걸린 시간을 구하고
-  distance = (dlapsed * 19000) / 2     # 초음파 속도를 이용해서 거리 계산
+  distance = (elapsed * 19000) / 2     # 초음파 속도를 이용해서 거리 계산
 
   return distance                       # 거리 반환
 
@@ -27,7 +27,7 @@ GPIO.setup(echoPin, GPIO.IN)
 
 try:
   while True:
-    distance = measure():
+    distance = measure()
     print("Distance: %.2f cm" %distance)
     time.sleep(1)
 
