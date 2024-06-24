3ab8330d4e774327bf6e38b93d2dcc10abd90379
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-24 14:21:24 +0900

    Update flask01.py

--- day03/flask01.py
+++ day03/flask01.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)    # name 이름을 통한 flask 객체 생성
 
-@app.route("/")
-def hello():
-  return "Hello World"
+@app.route("/")          # 라우팅을 위한 뷰함수 등록  # /로 요청하면
+def hello():             # hello 함수가 실행되어 
+  return "안녕하시렵니까!!"  # 화면에 글이 나타남
 
 if __name__=="__main__":
   app.run(host="0.0.0.0", debug=True)
