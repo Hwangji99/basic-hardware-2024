c5de04cb9f1a1ec0cfc62c1b0801b8367ae065cb
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-24 14:25:03 +0900

    Update flask01.py

--- day03/flask01.py
+++ day03/flask01.py
@@ -8,3 +8,4 @@ def hello():             # hello 함수가 실행되어
 
 if __name__=="__main__":    # 만약에 터미널에서 직접 실행 파일이 main이라는 함수로 바뀌면
   app.run(host="0.0.0.0", debug=True)  # 서버를 구동시켜라!!  # 실행을 위한 명령문으로 보면 된다
+                                       # debug=True 는 작업한 내용 서버에 바로바로 반영이 된다
