106b25762628398950b45a28ce93fc77c4d6b6b6
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-24 15:01:49 +0900

    Create flask03.py

---
+++ day03/flask03.py
@@ -0,0 +1,26 @@
+# URL 접속을 /led/on, /let/off로 접속하면 led를 on, off하는 웹페이지를 만들기
+import RPi.GPIO as GPIO
+import time
+
+from flask import Flask
+
+led = 19
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led, GPIO.OUT)
+try:
+  while True:
+    @app.route("/led/on")
+    def hello():
+      return "LED ON"
+    
+    @app.route("/led/off")
+    def hello():
+      return "LED OFF"
+    
+    if __name__ == "__main__":
+      app.run(host="0.0.0.0", port="10012", debug=True)
+      GPIO.output(led, False)
+
+except KeyboardInterrupt:  # Ctrl + c
+  GPIO.cleanup()
