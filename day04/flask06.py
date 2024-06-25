758d976c124ab46c6a73e6695079b002347be81d
HWANG JIHWAN <158007430+Hwangji99@users.noreply.github.com>
2024-06-25 09:54:25 +0900

    Update flask06.py

--- day04/flask06.py
+++ day04/flask06.py
@@ -1,11 +1,15 @@
 # 동일한 폴더 위치에 templates 폴더를 만들고 거기에 html 파일을 저장한다
 
+import RPi.GPIO as GPIO
 from flask import Flask, request, render_template
-from gpiozero import LED
-
-app = Flask(__name__)
+# from gpiozero import LED
 
 led = LED(21)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led, GPIO.OUT)
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
@@ -16,12 +20,15 @@ def data():
   data = request.form['led']
 
   if(data == 'on'):
-    led.on()
+    GPIO.output(led, False)
     return home()
 
   elif(data == 'off'):
-    led.off()
+    GPIO.output(led, True)
     return home()
 
 if __name__ == '__main__':
-  app.run(host = '0.0.0.0', port = '80')
+  try:
+    app.run(host = '0.0.0.0', port = '10101', debug=True)
+  except KeyboardInterrupt:
+    GPIO.cleanup()
